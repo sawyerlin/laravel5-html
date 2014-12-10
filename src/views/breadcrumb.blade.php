0e5febcb94779e1c987868ae83590ffe1f61a292
sawyerlin <sawyer.lin@gmail.com>
2014-12-10 10:38:51 +0100

    modify breadcrumb button style

--- src/views/breadcrumb.blade.php
+++ src/views/breadcrumb.blade.php
@@ -10,21 +10,21 @@
   @endforeach
   </div>
 
-  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+  <div class="col-xs-6 text-right">
   @foreach ($buttons as $button)
 
     {{-- ToDo: add switch statement --}}
     @if ($button['type'] === 'link')
 
-      {!! link_to_route_with_children($button['route'], _($button['name']), isset($button['datas']) ? $button['datas'] : array(), array('class' => 'btn btn-primary btn-large pull-right', 'role' => 'button'), '<span class="glyphicon glyphicon-'.$button['icon'].'" aria-hidden="true"></span>') !!} 
+      {!! link_to_route_with_children($button['route'], _($button['name']), isset($button['datas']) ? $button['datas'] : array(), array('class' => 'btn btn-primary btn-large', 'role' => 'button'), '<span class="glyphicon glyphicon-'.$button['icon'].'" aria-hidden="true"></span>') !!} 
 
     @elseif ($button['type'] === 'submit')
 
-      {!! Form::submit(_($button['name']), ['class' => 'btn btn-primary btn-large pull-right', 'role' => 'button']) !!} 
+      {!! Form::submit(_($button['name']), ['class' => 'btn btn-primary btn-large', 'role' => 'button']) !!} 
 
     @elseif ($button['type'] === 'toggle')
       
-      {!! link_to_toggle_with_children($button['id'], _($button['name']), array('class' => 'btn btn-primary btn-large pull-right', 'role' => 'button'), '<span class="glyphicon glyphicon-'.$button['icon'].'" aria-hidden="true"></span>') !!} 
+      {!! link_to_toggle_with_children($button['id'], _($button['name']), array('class' => 'btn btn-primary btn-large', 'role' => 'button'), '<span class="glyphicon glyphicon-'.$button['icon'].'" aria-hidden="true"></span>') !!} 
 
     @endif
 
