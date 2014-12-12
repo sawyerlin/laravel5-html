2b61b9f148f6c9ae4a78f697f27da55ffbe5d0c9
sawyerlin <sawyer.lin@gmail.com>
2014-12-12 09:46:20 +0100

    indent code

--- src/Huge/Laravel5Html/Laravel5HtmlServiceProvider.php
+++ src/Huge/Laravel5Html/Laravel5HtmlServiceProvider.php
@@ -6,68 +6,68 @@ use View;
 
 class Laravel5HtmlServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('huge/laravel5-html');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('huge/laravel5-html');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-    $this->registerLaravel5FormBuilder();
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerLaravel5FormBuilder();
 
-    $this->registerLaravel5HtmlBuilder();
+        $this->registerLaravel5HtmlBuilder();
 
-    $this->app->alias('laravel5-html.form', 'Illuminate\Html\Laravel5FormBuilder');
-    
-    $this->app->alias('laravel5-html.html', 'Illuminate\Html\Laravel5HtmlBuilder');
-	}
+        $this->app->alias('laravel5-html.form', 'Illuminate\Html\Laravel5FormBuilder');
 
-  protected function registerLaravel5FormBuilder() {
+        $this->app->alias('laravel5-html.html', 'Illuminate\Html\Laravel5HtmlBuilder');
+    }
 
-    $this->app->bindShared('laravel5-html.form', function($app) {
+    protected function registerLaravel5FormBuilder() {
 
-      $form = new Laravel5FormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
+        $this->app->bindShared('laravel5-html.form', function($app) {
 
-      return $form->setSessionStore($app['session.store']);
-    
-    });
+            $form = new Laravel5FormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
 
-  }
+            return $form->setSessionStore($app['session.store']);
 
-  protected function registerLaravel5HtmlBuilder() {
+        });
 
-    $this->app->bindShared('laravel5-html.html', function($app) {
+    }
 
-      return new Laravel5HtmlBuilder($app['url']);
+    protected function registerLaravel5HtmlBuilder() {
 
-    });
+        $this->app->bindShared('laravel5-html.html', function($app) {
 
-  }
+            return new Laravel5HtmlBuilder($app['url']);
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return ['laravel5-html.html', 'laravel5-html.form'];
-	}
+        });
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['laravel5-html.html', 'laravel5-html.form'];
+    }
 }
